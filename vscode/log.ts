d8bc66ea88bc1f4279b8d90c0582d8e9911526e1
darkgoldeagle <129761161+darkgoldeagle@users.noreply.github.com>
2023-07-18 23:50:42 +0430

    Delete log.ts

--- vscode/log.ts
+++
@@ -1,6 +0,0 @@
-const log = (message?: any, ...optionalparams : any[]) =>{ const showlog = true;
-    if (showlog) {
-        return console.log(message, ... optionalparams);
-    }
-};
-export default log;
